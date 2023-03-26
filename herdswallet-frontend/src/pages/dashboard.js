ddc6f011de179077061213820f7f33ba6b9fedf7
NoeDimitri <justind4@usf.edu>
2023-03-25 20:40:50 -0400

    cards updated

--- herdswallet-frontend/src/pages/dashboard.js
+++ herdswallet-frontend/src/pages/dashboard.js
@@ -1,11 +1,13 @@
 import {Helmet} from "react-helmet";
 import BrandNavbar from "../components/navbar.js"
+import CategoryCard from "../components/categoryCard.js"
 
 export default function Dashboard() {
     return(
         <>
             <Helmet> <title>Herds Wallet | Dashboard</title> </Helmet>
             <BrandNavbar/>
+            <CategoryCard name="shawty" amount = "300"/>
         </>
 
     )
